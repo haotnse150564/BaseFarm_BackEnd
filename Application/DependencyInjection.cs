e8afa3eebd207ff2f0a8cb721f209b11eafb9de7
NhatTruongFPT <truongtnse140532@fpt.edu.vn>
2025-02-25 09:54:27 +0700

    fix config + product list api

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -2,9 +2,12 @@
 using Application.Interfaces;
 using Application.Services;
 using Infrastructure.Mapper;
+using Infrastructure.Repositories.Implement;
+using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Application.Services.Implement;
 
 namespace Infrastructure;
 
@@ -13,13 +16,17 @@ public static class DependencyInjection
     public static IServiceCollection AddInfractstructure(this IServiceCollection services, IConfiguration config)
     {
         services.AddScoped<IUnitOfWorks, UnitOfWorks>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+
+        services.AddScoped<IProductServices, ProductServices>();
+
 
         #region Config Repository and Service
 
 
         #endregion
 
-        //services.AddSingleton<ICurrentTime, CurrentTime>();
+        services.AddSingleton<ICurrentTime, CurrentTime>();
         // Use local DB
         services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(config.GetConnectionString("Default")));
         services.AddAutoMapper(typeof(MapperConfigs).Assembly);
