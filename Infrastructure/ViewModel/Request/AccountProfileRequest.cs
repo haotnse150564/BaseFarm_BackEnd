16bbebae5b504e28c0a1b5c2f842941a1635e3be
Dang Nhat Huy <113001292+dang-nhat-huy@users.noreply.github.com>
2025-03-09 12:33:26 +0700

    update profile

---
+++ Infrastructure/ViewModel/Request/AccountProfileRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ViewModel.Request
+{
+    public class AccountProfileRequest
+    {
+        public class ProfileRequestDTO
+        {
+            [Required]
+            public long AccountId { get; set; }
+
+            [Range(0, 1, ErrorMessage = "Gender must be 0 or 1")]
+            public int? Gender { get; set; }
+
+            [Phone(ErrorMessage = "Invalid phone number format")]
+            public string? Phone { get; set; }
+
+            [Required(ErrorMessage = "Fullname is required")]
+            [MaxLength(100, ErrorMessage = "Fullname cannot exceed 100 characters")]
+            public string? Fullname { get; set; }
+
+            [MaxLength(255, ErrorMessage = "Address cannot exceed 255 characters")]
+            public string? Address { get; set; }
+            public string? Images { get; set; }
+        }
+    }
+}
