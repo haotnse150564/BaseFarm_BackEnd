b4b44d05836b47781dad4345c0ce18a16debccf2
haotnse150564 <anmay2k1@gmail.com>
2025-03-22 12:39:57 +0700

    add base schedule services

---
+++ Application/Services/IScheduleServices.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.ViewModel.Request;
+using Infrastructure.ViewModel.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public interface IScheduleServices
+    {
+        Task<ScheduleResponse> GetAllScheduleAsync(int pageIndex, int pageSize);
+        Task<ScheduleResponse> GetScheduleByIdAsync(long ScheduleId);
+        Task<ScheduleResponse> GetScheduleByNameAsync(string ScheduleName, int pageIndex, int pageSize);
+        Task<ScheduleResponse> CreateScheduleAsync(ScheduleRequest request);
+        Task<ScheduleResponse> UpdateScheduleById(long ScheduleId, ScheduleRequest request);
+        Task<ScheduleResponse> ChangeScheduleStatusById(long ScheduleId);
+    }
+}
---
+++ Infrastructure/ViewModel/Response/ScheduleResponse.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ViewModel.Response
+{
+    public class ScheduleResponse
+    {
+        public DateOnly? StartDate { get; set; }
+
+        public DateOnly? EndDate { get; set; }
+
+        public Status? Status { get; set; }
+
+        public DateOnly? CreatedAt { get; set; }
+
+        public DateOnly? UpdatedAt { get; set; }
+
+        public long AssignedTo { get; set; }
+
+        public int FarmActivityId { get; set; }
+
+        public long FarmDetailsId { get; set; }
+    }
+}
