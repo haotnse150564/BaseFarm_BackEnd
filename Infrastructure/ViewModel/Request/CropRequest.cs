ba13a8956980eed21c1b05b6c11b29e133031577
haotnse150564 <anmay2k1@gmail.com>
2025-05-08 13:38:48 +0700

    update Product check list, Create Crop

---
+++ Infrastructure/ViewModel/Request/CropRequest.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ViewModel.Request
+{
+    public class CropRequest
+    {
+        public string? CropName { get; set; }
+
+        public string? Description { get; set; }
+
+        public int? Quantity { get; set; }
+
+        public DateOnly? PlantingDate { get; set; }
+
+        public DateOnly? HarvestDate { get; set; }
+    }
+}
