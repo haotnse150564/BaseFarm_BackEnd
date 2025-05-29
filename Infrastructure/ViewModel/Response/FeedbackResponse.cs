d785e171337f850021c5f7be675b6ebbe940625b
Dang Nhat Huy <113001292+dang-nhat-huy@users.noreply.github.com>
2025-05-29 21:26:25 +0700

    adjust FeedbackResponse

--- Infrastructure/ViewModel/Response/FeedbackResponse.cs
+++ Infrastructure/ViewModel/Response/FeedbackResponse.cs
@@ -24,6 +24,7 @@ namespace Infrastructure.ViewModel.Response
 
         public class ViewFeedbackDTO
         {
+            public long FeedbackId { get; set; }
             public string? Comment { get; set; }
 
             public int? Rating { get; set; }
