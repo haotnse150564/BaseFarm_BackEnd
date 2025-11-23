af7b72b5e6b15b3cf08fccc38e8761c8fcf38055
Nhựt Hào <anmay2k1@gmail.com>
2025-11-23 16:00:14 +0700

    add services FarmEquipment

---
+++ Application/Services/Implement/FarmEquipmentServices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Implement
+{
+    public class FarmEquipmentServices : IFarmEquipmentServices
+    {
+        public Task<object> CreateFarmEquipment(object request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteFarmEquipment(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> GetFarmEquipmentByDevicesName(long farmId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> GetListEquipmentByFarmId(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> UpdateFarmEquipment(long id, object request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Application/Services/Implement/IFarmEquipmentServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Implement
+{
+    public interface IFarmEquipmentServices
+    {
+        Task<object> GetAll();
+        Task<object> GetListEquipmentByFarmId(long id);
+        Task<object> CreateFarmEquipment(object request);
+        Task<object> UpdateFarmEquipment(long id, object request);
+        Task<bool> DeleteFarmEquipment(long id);
+        Task<object> GetFarmEquipmentByDevicesName(long farmId);
+    }
+}
