47f323675dc722868ba094cc44b0cfca8b84bcc9
Dang Nhat Huy <113001292+dang-nhat-huy@users.noreply.github.com>
2026-01-06 13:06:22 +0700

    Update DependencyInjection.cs

--- WebAPI/DependencyInjection.cs
+++ WebAPI/DependencyInjection.cs
@@ -126,16 +126,16 @@ public static class DependencyInjection
         services.AddFluentValidationClientsideAdapters();
 
 
-        builder.Services.AddCors(options =>
+        services.AddCors(options =>
         {
-            options.AddPolicy(name: "_myAllowSpecificOrigins",
-                policy =>
-                {
-                    policy.WithOrigins("https://ifms.io.vn")  // Origin frontend
-                          .AllowAnyHeader()
-                          .AllowAnyMethod()
-                          .AllowCredentials();
-                });
+            options.AddPolicy("_myAllowSpecificOrigins", policy =>
+            {
+                policy
+                    .WithOrigins("http://localhost:5173", "https://ifms.io.vn") // FE port
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
         });
 
 
